2e61c1261ae9b0f287e2c98953054065774537c0
Joep Schuurkes <j19sch@gmail.com>
2020-04-25 15:02:26 +0200

    chapter 2

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,11 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little teapot!"))
+
+(println "Cleanliness is next to goldiness")
+
+(defn train 
+  []
+  (println "Choo choo!"))
+
+(map)
