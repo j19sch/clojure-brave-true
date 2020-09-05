5cafb0bea6f28781d57f5aa2ddda8e74a77a0515
Joep Schuurkes <j19sch@gmail.com>
2020-09-05 18:51:09 +0200

    adds part of hobbit program

---
+++ clojure-noob/src/clojure_noob/hobbit.clj
@@ -0,0 +1,64 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+; asym-hobbit-body-parts
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+; (matching-part {:name "elbow" :size 1})
+; (matching-part {:name "left-elbow" :size 1})
+; (matching-part {:name "right-elbow" :size 1})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+            (recur remaining
+                   (into final-body-parts
+                         (set [part (matching-part part)])))))))
+
+(symmetrize-body-parts asym-hobbit-body-parts)
+
+; (re-find #"^left-" "left-eye")
+; (re-find #"^left-" "cleft-chin")
+
+(reduce + [1 2 3 4])
+(reduce + 15 [1 2 3 4])
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+               (into final-body-parts (set [part (matching-part part)])))
+               []
+               asym-body-parts))
+
+
+(better-symmetrize-body-parts asym-hobbit-body-parts)
+
