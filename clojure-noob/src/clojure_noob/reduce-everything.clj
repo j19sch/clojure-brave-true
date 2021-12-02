f165d61b5f03c85b4e8af88abd122cbe404cc427
Joep Schuurkes <j19sch@gmail.com>
2021-12-02 15:27:05 +0100

    progress towards filter as reduce

--- clojure-noob/src/clojure_noob/reduce-everything.clj
+++ clojure-noob/src/clojure_noob/reduce-everything.clj
@@ -1,6 +1,19 @@
 (ns clojure-noob.core
    (:gen-class))
 
+; reduce without coll
+(reduce print [])
+; => nil
+
+; reduce without col but with coll
+(reduce print 1 [])
+; => 1
+
+; reduce with single item in coll (print 3) ;=> 3nil (reduce print [3])
+; => 3
+(reduce print [3 4])
+; => 3 4nil
+
 ; If you want an exercise that will really blow your hair back, try implementing map using reduce,
 ; and then do the same for filter and some after you read about them later in this chapter.
 
@@ -59,5 +72,114 @@
 (filter #(< % 4) numbers)
 ; => (1 2 3)
 
+(defn my-simple-filter [inp]
+  (reduce (fn [vect numb]
+            (if (< numb 4)
+              (concat vect [numb])
+              vect))
+          ()
+          inp
+          )
+  )
+
+(if (even? 5)
+  "yes"
+  "no"
+  )
+
+(my-simple-filter numbers)
+;=> (1 2 3)
+
+
+; https://stackoverflow.com/a/26560579
+(defn rec [f numbers acc]
+  (reduce f acc numbers))
+
+(rec + [1 2 3] 0)
+; => 6
+
+(defn plus [a b] (+ a b))
+(plus 2 3)
+
+(rec plus [1 2 3] 0)
+;=> 6
+
+(defn red-func-lt4 [vect numb]
+  (if (< numb 4)
+    (concat vect [numb])
+    vect))
+
+(red-func-lt4 [1 2] 3)
+;=> (1 2 3)
+
+(reduce red-func-lt4 () numbers)
+;=> (1 2 3 4 5)
+;=> (1 2 3) when not anon function in red-funct-lt4
+
+(defn my-filter-2 [func inp]
+  (reduce func () inp
+          )
+  )
+
+(my-filter-2 red-func-lt4 numbers)
+; => (1 2 3)
+
+(defn red-func [f vect numb]
+  (if f
+    (concat vect [numb])
+    vect))
+
+(red-func lt4 [1 2] 3)
+;=> (1 2 3)
+
+
 (defn my-filter [func inp]
-  (reduce ))
+  (reduce (fn [vect numb]
+            (if func
+              (concat vect [numb])
+              vect))
+          ()
+          inp
+  )
+)
+
+(defn my-filter [func inp]
+  ((defn f [vect numb]
+     (if func
+       (concat vect [numb])
+       vect))
+     (reduce f () inp)
+   )
+  )
+
+(defn two-lines [a b]
+  (print a)
+  (print b)
+  )
+(two-lines "one" "two")
+;=> onetwonil
+
+(defn lt4 [anumb] (< anumb 4))
+(lt4 3)
+;=> true
+(lt4 4)
+;=> false
+
+(my-filter lt4 numbers)
+;=> (1 2 3 4 5)
+
+(my-filter #(< % 4) numbers)
+;=> don't know how to create ISeq
+;=> (1 2 3 4 5) -> not sure why it did this -> got it back after putting func and inp in the right place
+; brackets and vim-sexp are messing with me, more sluprage and barfage!
+; hypothesis: func is evaluated not executed
+
+; filter demonstrated in reduce explanation
+(reduce (fn [vect numb]
+          (if (< numb 4)
+            (concat vect [numb])
+            vect))
+        () 
+        [1 2 3 4 5])
+;=> (1 2 3)
+;=> to vector is easier than list: conj, no [] around numb, val []
