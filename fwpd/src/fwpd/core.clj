a96211ce6b7ce33787ee796e9842bd49bfa98a9f
jss <j19sch@gmail.com>
2022-06-04 14:41:20 +0200

    adds the 4 exercises of chapter 4

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -79,3 +79,10 @@
   (filter #(>= (:glitter-index %) minimum-glitter) records))
 (glitter-filter 3 (mapify (parse (slurp filename))))
 ; => ({:name "Edward Cullen", :glitter-index 10} {:name "Jacob Black", :glitter-index 3} {:name "Carlisle Cullen", :glitter-index 6})
+
+; Exercises
+; Exercise 1. Turn the result of your glitter filter into a list of names.
+; Exercise 2. Write a function, append, which will append a new suspect to your list of suspects.
+; Exercise 3. Write a function, validate, which will check that :name and :glitter-index are present when you append. The validate function should accept two arguments: a map of keywords to validating functions, similar to conversions, and the record to be validated.
+; Exercise 4. Write a function that will take your list of maps and convert it back to a CSV string. You’ll need to use the clojure.string/join function.
+
